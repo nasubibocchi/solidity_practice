f5b4e30fda44a918f72dd7abe357d115bfee506a
"nasubibocchi" <"matsuhito125@gmail.com">
2022-06-19 18:24:43 +0900

    Destroy smart contract

--- contracts/SendMoneyExample.sol
+++ contracts/SendMoneyExample.sol
@@ -34,4 +34,9 @@ contract SendMoneyExample {
         require(!isPaused, "The Contract is paused");
         _to.transfer(this.getBalance());
     }
+
+    function destroySmartContract(address payable _to) public {
+        require(msg.sender == owner, "You are not the owner");
+        selfdestruct(_to);
+    }
 }
