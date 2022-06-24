6e670e2ca90d69d44ee1ce95ed360669111562ff
"nasubibocchi" <"matsuhito125@gmail.com">
2022-06-24 17:30:51 +0900

    Enable to initialize owner address and destroy contract

--- contracts/FunctionExample.sol
+++ contracts/FunctionExample.sol
@@ -1,22 +1,36 @@
-pragma solidity ^0.5.13;
+pragma solidity ^0.8.3;
 
 contract ExceptionExample {
-    mapping (address => uint64) public balanceReceived;
+    mapping (address => uint) public balanceReceived;
 
-    function sendMoney() public payable {
-        assert(msg.value == uint64(msg.value));
-        balanceReceived[msg.sender] += uint64(msg.value);
-        assert(balanceReceived[msg.sender] >= uint64(msg.value));
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
     }
 
-    function withdrawMoney(address payable _to, uint64 _amount) public {
+    function destroySmartContract() public {
+        require(msg.sender == owner, "You are not the owner");
+        selfdestruct(owner);
+    }
+
+    function receiveMoney() public payable {
+        // assert(msg.value == uint64(msg.value));
+        // balanceReceived[msg.sender] += uint64(msg.value);
+        // assert(balanceReceived[msg.sender] >= uint64(msg.value));
+
+        assert(balanceReceived[msg.sender] + msg.value >= balanceReceived[msg.sender]);
+        balanceReceived[msg.sender] += msg.value;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public {
         require(_amount <= balanceReceived[msg.sender], "Not enough ether");
         assert(balanceReceived[msg.sender] >= balanceReceived[msg.sender] - _amount);
         balanceReceived[msg.sender] -= _amount;
         _to.transfer(_amount);
     }
 
-    function () external payable {
-        sendMoney();
+    receive() external payable {
+        receiveMoney();
     }
 }
