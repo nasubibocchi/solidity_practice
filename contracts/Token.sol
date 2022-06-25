3a34865570195033449691c534d8f1744a4164b7
"nasubibocchi" <"matsuhito125@gmail.com">
2022-06-25 11:59:21 +0900

    Define event and implement it

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,6 +6,8 @@ contract Token is Owned {
 
     mapping(address => uint) public tokenBalance;
 
+    event TokensSent(address _from, address _to, uint _amount);
+
     uint tokenPrice = 1 ether;
 
     constructor() {
@@ -26,12 +28,16 @@ contract Token is Owned {
         tokenBalance[msg.sender] += msg.value / tokenPrice;
     }
 
-    function sendToken(address _to, uint _amount) public {
+    function sendToken(address _to, uint _amount) public returns(bool) {
         require(tokenBalance[msg.sender] >= _amount, "Not enough tokens");
         assert(tokenBalance[_to] + _amount >= tokenBalance[_to]);
         assert(tokenBalance[msg.sender] - _amount <= tokenBalance[msg.sender]);
         tokenBalance[msg.sender] -= _amount;
         tokenBalance[_to] += _amount;
+
+        emit TokensSent(msg.sender, _to, _amount);
+
+        return true;
     }
 
 }
