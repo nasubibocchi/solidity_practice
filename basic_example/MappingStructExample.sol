5a2f4aecd3f5dc0e9f1b02d3313cbaa57d0803a5
"nasubibocchi" <"matsuhito125@gmail.com">
2022-06-26 18:14:06 +0900

    Rename directory

---
+++ basic_example/MappingStructExample.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.13;
+
+contract MappingStructExample {
+    struct Payment {
+        uint amount;
+        uint timestamps;
+    }
+
+    struct Balance {
+        uint totalBalance;
+        uint numPayment; // number of payment times
+        mapping (uint => Payment) payments; 
+    }
+
+    mapping (address => Balance) public balanceRecieved;
+    
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function sendMoney() public payable {
+        balanceRecieved[msg.sender].totalBalance += msg.value;
+
+        Payment memory payment = Payment(msg.value, now);
+
+        // define payments as property of Balance
+        balanceRecieved[msg.sender].payments[balanceRecieved[msg.sender].numPayment] = payment;
+        // define numPayment as property of Balance
+        balanceRecieved[msg.sender].numPayment++;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require(balanceRecieved[msg.sender].totalBalance >= _amount, "Not enough funds");
+        balanceRecieved[msg.sender].totalBalance -= _amount;
+        _to.transfer(_amount); 
+    }
+
+    function withdrawAllMoney(address payable _to) public {
+        uint balanceToSend = balanceRecieved[msg.sender].totalBalance;
+        balanceRecieved[msg.sender].totalBalance = 0; 
+        _to.transfer(balanceToSend);
+    }
+}
---
+++ basic_example/SendMoneyExample.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.5.13;
+
+contract SendMoneyExample {
+    address owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    bool isPaused; // it is false if not initialized
+
+    function setIsPaused(bool _isPaused) public {
+        require(msg.sender == owner, "You are not the owner");
+        isPaused = _isPaused;
+    }
+
+    uint public balanceReceived;
+
+    function receiveMoney() public payable {
+        balanceReceived += msg.value;
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function withdrawMoney() public {
+        address payable to = msg.sender;
+        to.transfer(this.getBalance());
+    }
+
+    function withdrawMoneyTo(address payable _to) public {
+        require(msg.sender == owner, "You are not the owner");
+        require(!isPaused, "The Contract is paused");
+        _to.transfer(this.getBalance());
+    }
+
+    function destroySmartContract(address payable _to) public {
+        require(msg.sender == owner, "You are not the owner");
+        selfdestruct(_to);
+    }
+}
