8a28476f669d1fb8906c4ba06675207bc098099f
"nasubibocchi" <"matsuhito125@gmail.com">
2022-06-26 13:03:12 +0900

    To be able to withdraw money by owner

--- shared_wallet/SharedWallet.sol
+++ shared_wallet/SharedWallet.sol
@@ -7,15 +7,20 @@ contract SharedWallet {
         owner = payable(msg.sender);
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not allowed");
+        _;
+    }
+
     mapping (address => uint) public totalBalance;
 
     function sendMoney() public payable {
         totalBalance[msg.sender] += msg.value;
     }
 
-    function withdrawMoney(address payable _from, address payable _to, uint _amount) public {
-        require(totalBalance[_from] >= _amount, "Not enough money");
-        totalBalance[_from] -= _amount;
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+        require(totalBalance[msg.sender] >= _amount, "Not enough money");
+        totalBalance[msg.sender] -= _amount;
         _to.transfer(_amount);
     }
 
