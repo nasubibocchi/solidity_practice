98e6e37df9beaf47358b95a1f16a153b0af40165
"nasubibocchi" <"matsuhito125@gmail.com">
2022-07-01 18:29:01 +0900

    Enable to add allowance by owner

--- shared_wallet/SharedWallet.sol
+++ shared_wallet/SharedWallet.sol
@@ -15,20 +15,41 @@ contract SharedWallet is Ownable {
     //     _;
     // }
 
+    // create isOwner() because Ownable.sol does not define this function(?)
+    function isOwner() public view returns(bool) {
+        return msg.sender == owner();
+    }
+
     // mapping (address => uint) public totalBalance;
 
     // function sendMoney() public payable {
     //     totalBalance[msg.sender] += msg.value;
     // }
 
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+    mapping(address => uint) public allowance;
+
+    // add allowance to withdraw money(amount) by owner
+    function addAllowance(address _who, uint _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[msg.sender] > _amount, "You are not allowed");
+        _;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         // require(totalBalance[msg.sender] >= _amount, "Not enough money");
         // totalBalance[msg.sender] -= _amount;
         _to.transfer(_amount);
     }
 
+    // work with v.0.8.13 
     fallback() external payable {
         // following process is not necessary because fallback() function play the role
         // sendMoney();
     }
+
+    // work with v.0.5.13 
+    // function () external payable {}
 }
